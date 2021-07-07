687e879978186fe10cf995a7e06dcf99087fadef
Jeff Thomassen <jeff.thomassen@ucdenver.edu>
2021-07-06 19:37:13 -0600

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,9 @@
+function introduction(name=Jeff) {
+    return (`Hi, my name is ${name}.`);
+}
+function introductionWithLanguage(name="Jeff", language="JavaScript") {
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`);
+}
+function introductionWithLanguageOptional(name="Jeff", language="JavaScript") {
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`);
+}
